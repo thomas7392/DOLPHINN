049361005103420bd4c52821d16d7e6e02c0d4c5
thomas7392 <thomas1gold@hotmail.com>
2023-06-30 21:14:49 +0200

    Add time-optimal objective

--- DOLPHINN/objectives.py
+++ DOLPHINN/objectives.py
@@ -1,3 +1,8 @@
+## THomas GOldman 2023
+# DOLPHINN
+
+import numpy as np
+
 import deepxde as dde
 from deepxde.backend import tf
 import tensorflow_probability as tfp
@@ -7,7 +12,10 @@ from .function import Function
 
 class OptimalFuel(Function):
 
-    def __init__(self, data):
+    def __init__(self,
+                data):
+
+        self._entries = len(data['initial_state'])
         super().__init__(data)
 
     def call(self, t, y, losses):
@@ -18,7 +26,9 @@ class OptimalFuel(Function):
 
         # Get control and calculate norm
         t = tf.reshape(t, (1, -1))[0] * self.time_scale
-        U = y[:, 4:6]
+
+        U = y[:, self._entries:]
+
         U_norm = tf.norm(U, axis=1)
 
         # Sort time and control
@@ -30,3 +40,28 @@ class OptimalFuel(Function):
         propellent_mass = (1/self.isp/9.81) * tfp.math.trapz(U_norm_sorted, t_sorted)
 
         return propellent_mass
+
+
+class OptimalTime(Function):
+
+    def __init__(self,
+                data):
+
+        self._entries = len(data['final_state'])
+        self._initial_tensor = tf.convert_to_tensor(np.array([data['final_state']]), tf.float32)
+        super().__init__(data)
+
+    def call(self, t, y, losses):
+        '''
+        Calculate consumed mass by integrating the thrust profile.
+        Requires a whole batch of input/output pairs.
+        '''
+
+        # Get control and calculate norm
+        states = y[:, :self._entries]
+        diffs = tf.subtract(states, self._initial_tensor)
+        squared_diffs  = tf.square(diffs)
+        column_averages = tf.reduce_mean(squared_diffs, axis=0)
+        sum_averages = tf.reduce_sum(column_averages)
+
+        return sum_averages
