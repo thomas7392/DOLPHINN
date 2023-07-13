039b6008ce657cba0fb142eedf82a8e13d991e5a
thomas7392 <thomas1gold@hotmail.com>
2023-07-13 22:45:21 +0200

    Add fuel and final radius metrics

--- DOLPHINN/metrics.py
+++ DOLPHINN/metrics.py
@@ -6,6 +6,8 @@ from . import utils
 from . import coordinate_transformations
 from abc import ABC, abstractmethod
 
+from scipy.integrate import trapezoid
+
 
 class Metric(ABC):
     '''
@@ -26,6 +28,9 @@ class Metric(ABC):
         self.DOLPHINN = DOLPHINN
         self.coordinates = self.DOLPHINN.dynamics.coordinates
         self.DOLPHINN._metric_verification_epoch = -1
+
+        self.DOLPHINN._states_for_metric, self.DOLPHINN._verification_states_for_metric,\
+        self.DOLPHINN._masses_for_metric, self.DOLPHINN._verification_masses_for_metric = None, None, None, None
 
     @abstractmethod
     def call(self):
@@ -61,7 +66,7 @@ class Metric(ABC):
                 self.DOLPHINN._masses_for_metric = _states[:,-1]
                 _states = _states[:,:-1]
 
-            if self.coordinates == "radial":
+            if self.coordinates == "radial" and not self.DOLPHINN.dynamics.theta:
                 theta = utils.integrate_theta(times[:,0],
                                                 _states)
                 _states = np.concatenate((_states[:,0:1], theta.reshape(-1, 1), _states[:,1:]), axis = 1)
@@ -118,7 +123,7 @@ class FinalDv(Metric):
 
         # Get the verification information
         states, verification_states, _, _ = self.run_verification(train_state)
-        return np.linalg.norm(states[-1,1:3] - verification_states[-1,1:3])
+        return np.linalg.norm(states[-1,3:5] - verification_states[-1,3:5])
 
 class FinalDm(Metric):
     '''
@@ -132,6 +137,78 @@ class FinalDm(Metric):
         # Get the verification information
         _, _, masses, verification_masses = self.run_verification(train_state)
         return masses[-1] - verification_masses[-1]
+
+
+
+class FuelTUDAT(Metric):
+    '''
+    Fuel use metric
+    [Warning] This metric requires the propagation of fuel.
+    '''
+
+    def __init__(self, DOLPHINN):
+        super().__init__(DOLPHINN)
+
+    def call(self, train_state):
+
+        if not self.DOLPHINN.dynamics.mass:
+            raise Exception("[DOLPHINN] Attemped to calculate fuel metric from vehicle mass, but mass rate is not modelled")
+
+        # Get the verification information
+        _, _, _, verification_masses = self.run_verification(train_state)
+        return self.DOLPHINN.data['m'] - verification_masses[-1]
+
+
+class Fuel(Metric):
+    '''
+    Final velocity difference metric
+    '''
+    def __init__(self, DOLPHINN):
+        super().__init__(DOLPHINN)
+
+    def call(self, train_state):
+
+        # Get test states, strip mass if included
+        if self.DOLPHINN.dynamics.mass:
+            states = train_state.y_pred_test[:,:-1]
+        else:
+            states = train_state.y_pred_test
+
+        # Get test times
+        t = train_state.X_test.reshape(1, -1)[0] * self.DOLPHINN.data['time_scale']
+
+        # Calculate Thrust values. Assume is in Newtons.
+        U = states[:,-self.DOLPHINN.dynamics.control_entries:]
+        U_norm = np.linalg.norm(U, axis=1)
+
+        # Sort time and control
+        idx = np.argsort(t)
+        t_sorted = np.take(t, idx)
+        U_norm_sorted = np.take(U_norm, idx)
+
+        # Propellent mass
+        propellent_mass = (1/self.DOLPHINN.data['isp']/9.81) * trapezoid(U_norm_sorted, t_sorted)
+
+        return propellent_mass
+
+
+
+class FinalRadius(Metric):
+    '''
+    Final velocity difference metric
+    '''
+    def __init__(self, DOLPHINN):
+        super().__init__(DOLPHINN)
+
+    def call(self, train_state):
+
+        # Get the verification information
+        states, _, _, _ = self.run_verification(train_state)
+        return np.linalg.norm(states[-1,1:3])
+
+
+
+
 
 
 
