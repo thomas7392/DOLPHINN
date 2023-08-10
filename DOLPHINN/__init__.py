66ca258b9396726094ee70b63b3dba60aced47ad
thomas7392 <thomas1gold@hotmail.com>
2023-08-10 14:25:50 +0200

    Import functions and classes individually in init file

--- DOLPHINN/__init__.py
+++ DOLPHINN/__init__.py
@@ -1,15 +1,41 @@
 ## Thomas Goldman 2023
 # DOLPHINN
 
-from callbacks import .
-from coordinate_transformations import .
-from dynamics import .
-from input_layers import .
-from metrics import .
-from objectives import .
-from output_layers import .
+from callbacks import (SaveBest, StoreAnimationData)
+
+from coordinate_transformations import (NDcartesian_to_radial,
+                                        radial_to_NDcartesian,
+                                        cartesian_to_NDcartesian,
+                                        NDcartesian_to_cartesian,
+                                        radial_to_cartesian,
+                                        cartesian_to_radial)
+
+from dynamics import (TwoBodyProblemNoneDimensional,
+                      TwoBodyProblemNonDimensionalControl,
+                      TwoBodyProblemRadialNonDimensional,
+                      TwoBodyProblemRadialNonDimensionalControl,
+                      TwoBodyProblemRadialNonDimensionalControl_mass,
+                      TwoBodyProblemRadialThetaNonDimensionalControl_mass,
+                      )
+
+from metrics import (FinalDr, FinalDv, FinalDm, FinalRadius, Fuel, FuelTUDAT)
+from objectives import (OptimalFuel, OptimalTime, OptimalFinalMass, MaximumRadius)
+
+from output_layers import (InitialStateLayer,
+                           InitialFinalStateLayer_Cartesian,
+                           InitialFinalStateLayer_Radial,
+                           InitialFinalStateLayer_Radial_tanh,
+                           InitialFinalStateLayer_Radial_tanh_mass,
+                           InitialFinalStateLayer_Radial2_tanh_mass,
+                           InitialFinalStateLayer_RadialTheta_tanh_mass)
+
 from pinn import DOLPHINN
-from plotting import .
-from training import .
-from utils import .
-from verification import .
+from plotting import (plot_coordinates, plot_loss, plot_metrics, plot_transfer, compare, compare_mass)
+from training import (Scheduler, Restarter, Restorer)
+
+from utils import (get_dynamics_info_from_config,
+                   integrate_theta,
+                   path_or_instance,
+                   print_config)
+
+from verification import Verification
