157b7420e1fe3c6c7ac7d897e4ce0e8204205754
thomas7392 <thomas1gold@hotmail.com>
2023-08-10 14:27:54 +0200

    Add . to import statements

--- DOLPHINN/__init__.py
+++ DOLPHINN/__init__.py
@@ -1,16 +1,16 @@
 ## Thomas Goldman 2023
 # DOLPHINN
 
-from callbacks import (SaveBest, StoreAnimationData)
+from .callbacks import (SaveBest, StoreAnimationData)
 
-from coordinate_transformations import (NDcartesian_to_radial,
+from .coordinate_transformations import (NDcartesian_to_radial,
                                         radial_to_NDcartesian,
                                         cartesian_to_NDcartesian,
                                         NDcartesian_to_cartesian,
                                         radial_to_cartesian,
                                         cartesian_to_radial)
 
-from dynamics import (TwoBodyProblemNoneDimensional,
+from .dynamics import (TwoBodyProblemNoneDimensional,
                       TwoBodyProblemNonDimensionalControl,
                       TwoBodyProblemRadialNonDimensional,
                       TwoBodyProblemRadialNonDimensionalControl,
@@ -18,10 +18,10 @@ from dynamics import (TwoBodyProblemNoneDimensional,
                       TwoBodyProblemRadialThetaNonDimensionalControl_mass,
                       )
 
-from metrics import (FinalDr, FinalDv, FinalDm, FinalRadius, Fuel, FuelTUDAT)
-from objectives import (OptimalFuel, OptimalTime, OptimalFinalMass, MaximumRadius)
+from .metrics import (FinalDr, FinalDv, FinalDm, FinalRadius, Fuel, FuelTUDAT)
+from .objectives import (OptimalFuel, OptimalTime, OptimalFinalMass, MaximumRadius)
 
-from output_layers import (InitialStateLayer,
+from .output_layers import (InitialStateLayer,
                            InitialFinalStateLayer_Cartesian,
                            InitialFinalStateLayer_Radial,
                            InitialFinalStateLayer_Radial_tanh,
@@ -29,13 +29,13 @@ from output_layers import (InitialStateLayer,
                            InitialFinalStateLayer_Radial2_tanh_mass,
                            InitialFinalStateLayer_RadialTheta_tanh_mass)
 
-from pinn import DOLPHINN
-from plotting import (plot_coordinates, plot_loss, plot_metrics, plot_transfer, compare, compare_mass)
-from training import (Scheduler, Restarter, Restorer)
+from .pinn import DOLPHINN
+from .plotting import (plot_coordinates, plot_loss, plot_metrics, plot_transfer, compare, compare_mass)
+from .training import (Scheduler, Restarter, Restorer)
 
-from utils import (get_dynamics_info_from_config,
+from .utils import (get_dynamics_info_from_config,
                    integrate_theta,
                    path_or_instance,
                    print_config)
 
-from verification import Verification
+from .verification import Verification
