33bedfbfea234ecb991339ba7ce83391d16edc6f
thomas7392 <thomas1gold@hotmail.com>
2023-08-10 13:26:10 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DOLPHINN',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [],
+    },
+)
